f311952ae70eb0c1eec98bb9d4a6278a69a7c915
pavithrra04 <61133079+pavithrra04@users.noreply.github.com>
2020-02-16 22:51:33 -0800

    Add files via upload

---
+++ select.py
@@ -0,0 +1,14 @@
+i=1
+while i<5:
+    a=input('enter the value')
+    if a=='1':
+        print('go to clg')
+    if a=='2':
+        print('go to vacation')
+    if a=='3':
+        print('take test')
+    if a=='4':
+        print('play game')
+    if a=='q':
+        print('quit')
+        break
